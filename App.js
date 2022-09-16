91e9a9e0538b19c2e2ca4b873c83803e1a8e77d7
gustavogfranca <gustavo.gouveia.franca@gmail.com>
2022-09-16 09:27:06 -0300

    teste

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={{ uri: 'https://avatars.githubusercontent.com/u/108163840?v=4' }} style={styles.logo} />
+      <Text style={styles.instructions}>
+      </Text>
+
+      <TouchableOpacity onPress={() => alert('Teste')} style={styles.button}>
+        <Text style={styles.buttonText}>Thrumbo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +18,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f5f7ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 150,
+    height: 159,
+    marginBottom: 20,
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: '#3274',
+    padding: 20,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: '#fff',
+  }, 
 });
